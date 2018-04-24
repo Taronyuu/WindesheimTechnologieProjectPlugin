382d52e788e418e57b92a4f0a8436409f76aa026
Zander van der Meer <zander.meer@gmail.com>
2018-04-24 18:17:35 +0200

    Initial commit

---
+++ src/nl/windesheim/codeparser/OpenDialog.java
@@ -0,0 +1,12 @@
+package nl.windesheim.codeparser;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class OpenDialog extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
